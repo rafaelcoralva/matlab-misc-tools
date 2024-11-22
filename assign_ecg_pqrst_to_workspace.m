2c62618aafdd38b15a8402199d8d3a900c29cfe1
Rafael Cordero Alvarez <81193219+rafaelcoralva@users.noreply.github.com>
2024-11-23 00:41:48 +0100

    File relocation
    Moved to CardioLab folder

--- assign_ecg_pqrst_to_workspace.m
+++
@@ -1,4 +0,0 @@
-function assign_ecg_pqrst_to_workspace(assigned_var)
-% Function to assign the 'assigned_var' to the base workspace and name it 'num_cycles'.
-assignin('base','out_ecg_analysis_pqrst',assigned_var)
-end
